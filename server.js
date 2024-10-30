2231d08ddacc9f42dec7e0bf9a7b22b1233215ae
archer <econarcher2010@gmail.com>
2024-10-30 16:01:06 +0800

    finished

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/urlShortener");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrl = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrls", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.param.shortUrl });
+
+  if (shortUrl == null) {
+    return res.senStatus(404);
+  }
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 5656);
